704ad6641dc4c8cd7cde4e33176985232aa65a9f
Changhoon Kang <chkang@postech.ac.kr>
2021-12-10 17:36:58 +0900

    Add TRMS calculation

--- chaincode/dairy/go/dairy.go
+++ chaincode/dairy/go/dairy.go
@@ -27,8 +27,10 @@ type Commodity struct {
 
 type Participant struct {
 	ID              	string 		`json:"id"`
+	Trust_score     	float32 	`json:"trust_score"`
 	Reputation_score    float32 	`json:"reputation_score"`
 	Device_IDs    		[]string 	`json:"device_ids"`
+	Com_IDs			[]string	`json:"com_ids"`
 }
 
 type Device struct {
@@ -76,10 +78,10 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 	}
 
 	participants := []Participant{
-		Participant{ID: "PART1", Reputation_score: 1, Device_IDs: []string{"DEV1"}},
-		Participant{ID: "PART2", Reputation_score: 1, Device_IDs: []string{"DEV2"}},
-		Participant{ID: "PART3", Reputation_score: 2, Device_IDs: []string{"DEV3"}},
-		Participant{ID: "PART4", Reputation_score: 3, Device_IDs: []string{"DEV4"}},
+		Participant{ID: "PART1", Trust_score: 1, Reputation_score: 1, Device_IDs: []string{"DEV1"}, Com_IDs: []string{"COM1"}},
+		Participant{ID: "PART2", Trust_score: 1, Reputation_score: 1, Device_IDs: []string{"DEV2"}, Com_IDs: []string{"COM2"}},
+		Participant{ID: "PART3", Trust_score: 1, Reputation_score: 2, Device_IDs: []string{"DEV3"}, Com_IDs: []string{"COM3"}},
+		Participant{ID: "PART4", Trust_score: 1, Reputation_score: 3, Device_IDs: []string{"DEV4"}, Com_IDs: []string{"COM4"}},
 	}
 
 	for i, part := range participants {
@@ -111,6 +113,19 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 }
 
 func (s *SmartContract) CreateCom(ctx contractapi.TransactionContextInterface, id string, score float32, owner string, readings float32, ideal float32) error {
+	
+	part, err := s.QueryPart(ctx, owner)
+
+	if err != nil {
+		return err
+	}
+
+	part.Com_IDs = append(part.Com_IDs, id)
+
+	partAsBytes, _ := json.Marshal(part)
+
+	ctx.GetStub().PutState(owner, partAsBytes)
+	
 	com := Commodity{
 		ID: id,
 		Trust_score: score,
@@ -125,11 +140,13 @@ func (s *SmartContract) CreateCom(ctx contractapi.TransactionContextInterface, i
 	return ctx.GetStub().PutState(id, comAsBytes)
 }
 
-func (s *SmartContract) CreatePart(ctx contractapi.TransactionContextInterface, id string, score float32) error {
+func (s *SmartContract) CreatePart(ctx contractapi.TransactionContextInterface, id string, tscore float32, rscore float32) error {
 	part := Participant{
 		ID: id,
-		Reputation_score: score,
+		Trust_score: tscore,
+		Reputation_score: rscore,
 		Device_IDs: []string{},
+		Com_IDs: []string{},
 	}
 
 	partAsBytes, _ := json.Marshal(part)
@@ -294,15 +311,84 @@ func (s *SmartContract) QueryAll(ctx contractapi.TransactionContextInterface) (Q
 
 // Trade
 func (s *SmartContract) TradeCom(ctx contractapi.TransactionContextInterface, comNumber string, newOwner string) error {
+	var gamma float32 = 0.5
+	psai := []float32{1, 1, -1, 1, -1}
+	tc := []float32{0.92, 0.85 0.73, 0.67, 0.81}
+
+	var wt float32 = 0.3
+	var wT float32 = 0.4
+	var we float32 = 0.3
+
+	en := []float32{0.82, 0.74, 0.91, 0.88}
+
 	com, err := s.QueryCom(ctx, comNumber)
 
 	if err != nil {
 		return err
 	}
 
+	var owner string = com.Owners[len(com.Owners)-1]
 	com.Owners = append(com.Owners, newOwner)
-
 	comAsBytes, _ := json.Marshal(com)
+
+	// Updating Trust and Reputation scores
+	part1, _ := s.QueryPart(ctx, owner)
+	var tn1 float32 = 0
+	var tn2 float32 = com.Trust_score
+	for _, com_id := range part1.Com_IDs {
+		if com_id == comNumber {
+			continue
+		}
+		com, _ = s.QueryCom(ctx, com_id)
+		tn1 = tn1 + com.Trust_score
+	}
+	tn1 = tn1 / (len(part1.Com_IDs) - 1)
+
+	var sigma float32 = 0
+	for i := 0; i<len(psai); i++ {
+		sigma = sigma + psai[i] * tc[i]
+	}
+	sigma = sigma / len(psai)
+	part1.Trust_score = part1.Trust_score * gamma + (1 - gamma) * sigma
+
+	var En float32 = 0
+	var cor float32 = 1
+	for i := len(en)-1; i>=0; i-- {
+		En = En + en[i] * cor
+		cor = cor * gamma
+	}
+	En = En * (1 - gamma)
+
+	part1.Reputation_score = wt * tn1 + wT * part1.Trust_score + we * En
+	part1AsBytes, _ := json.Marshal(part1)
+	ctx.GetStub().PutState(owner, part1AsBytes)
+
+	// New Owner
+	part2, _ := s.QueryPart(ctx, newOwner)
+	for _, com_id := range part2.Com_IDs {
+		com, _ = s.QueryCom(ctx, com_id)
+		tn2 = tn2 + com.Trust_score
+	}
+	tn2 = tn2 / (len(part2.Com_IDs) + 1)
+
+	sigma = 0
+	for i := 0; i<len(psai); i++ {
+		sigma = sigma + psai[i] * tc[i]
+	}
+	sigma = sigma / len(psai)
+	part2.Trust_score = part2.Trust_score * gamma + (1 - gamma) * sigma
+
+	En = 0
+	cor = 1
+	for i := len(en)-1; i>=0; i-- {
+		En = En + en[i] * cor
+		cor = cor * gamma
+	}
+	En = En * (1 - gamma)
+
+	part2.Reputation_score = wt * tn2 + wT * part2.Trust_score + we * En
+	part2AsBytes, _ := json.Marshal(part2)
+	ctx.GetStub().PutState(newOwner, part2AsBytes)
 
 	return ctx.GetStub().PutState(comNumber, comAsBytes)
 }
